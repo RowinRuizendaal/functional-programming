dbbf1ba20907938365d9464dac1d8f17aa6bf322
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2020-10-21 19:07:26 +0200

    update wiki

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # functional-programming
-HVA functional-programming
+
+
+## About
+
+This is the reprository for cleaning the datavisualistaion students dataset formated in JSON, 
+
+
+## Installing and using the project
+
+**To run this project you need to have a dataset avaible in the directory**
+
+dataset/some_dataset.json or change the:
+
+```js
+const dataset = require('./dataset/Survey_Information_Design_clean-parsed.json');
+```
+
+Located in index.js, for privacy reasons I am not allowed to share this one
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev || yarn nodemon
+```
+
+
+### Lints and fixes files
+
+Fix all lint issues with just one command
+
+```
+npm run lint
+```
+
+### License
+
+Authtor: [RowinRuizendaal](https://github.com/rowinruizendaal), license: [mit](https://github.com/RowinRuizendaal/functional-programming/LICENSE)
+
--- index.js
+++ index.js
@@ -1,54 +1,48 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 require('dotenv').config();
+// eslint-disable-next-line max-len
 const dataset = require('./dataset/Survey_Information_Design_clean-parsed.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(`${__dirname}/public`));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.get('/bedtijd', (req, res) => {
-    const bedtijden = []
+  const bedtijden = dataset
+      .map((convertime) => {
+        return moment(`${convertime.bedtijd}`, ['h:mm A']).format('HH:mm');
+      });
 
-    for (let i = 0; i < dataset.length; i ++) {
-        const number = moment(`${dataset[i].bedtijd}`, ["h:mm A"]).format("HH:mm");
-        bedtijden.push(number)
-    }
-    return res.json(bedtijden)
-  })
+  return res.json(bedtijden);
+});
 
-  app.get('/oogkleur', (req, res) => {
-    // const hexvalues = dataset.map(hexvalue => {
-        
-    //     return hexvalue.oogKleur
-    // })
-
-    const hexvalues = dataset
-    .filter(hexvalue => hexvalue.oogKleur.match( /[0-9A-Fa-f]{6}/g))
-    .map(hexvalue => {
-        
-        if (!hexvalue.oogKleur.startsWith('#')) { //Place a # for every string
-            hexvalue.oogKleur = `#${hexvalue.oogKleur}`
+app.get('/oogkleur', (req, res) => {
+  const hexvalues = dataset
+      .filter((hexvalue) => hexvalue.oogKleur.match(/[0-9A-Fa-f]{6}/g))
+      .map((hexvalue) => {
+        if (!hexvalue.oogKleur.startsWith('#')) { // Place a # for every string
+          hexvalue.oogKleur = `#${hexvalue.oogKleur}`;
         }
 
-            if (hexvalue.oogKleur.includes(' ')) { // detect space
-            hexvalue.oogKleur = hexvalue.oogKleur.replace(/\s/g,'') //s --> space //g --> replace globally
+        if (hexvalue.oogKleur.includes(' ')) { // detect space
+          // eslint-disable-next-line max-len
+          hexvalue.oogKleur = hexvalue.oogKleur.replace(/\s/g, ''); // s --> space //g --> replace globally
         }
 
-        return hexvalue.oogKleur
-    })
+        return hexvalue.oogKleur;
+      });
 
-    console.log(hexvalues)
-    return res.json(hexvalues)
-})
-
-
+  console.log(hexvalues);
+  return res.json(hexvalues);
+});
 
 
-app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
